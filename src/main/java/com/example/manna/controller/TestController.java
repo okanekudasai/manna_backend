a3937d91fc81f0af04ef83fe6c802bfac09ab97d
okane <okanekudasai6@gmail.com>
2024-02-18 21:24:12 +0900

    ConnectMySQLServer

--- src/main/java/com/example/manna/controller/TestController.java
+++ src/main/java/com/example/manna/controller/TestController.java
@@ -1,13 +1,29 @@
 package com.example.manna.controller;
 
+import com.example.manna.entity.Test;
+import com.example.manna.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/test")
 public class TestController {
-    @GetMapping("/test")
+
+    @Autowired
+    TestRepository testRepository;
+
+    @GetMapping("/hello")
     String test() {
         System.out.println("hello world");
-        return "hello world";
+        return "hello world!";
+    }
+
+    @GetMapping("/getAllTest")
+    public List<Test> getAllTest() {
+        return testRepository.findAll();
     }
 }
---
+++ src/main/java/com/example/manna/entity/Test.java
@@ -0,0 +1,25 @@
+package com.example.manna.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
