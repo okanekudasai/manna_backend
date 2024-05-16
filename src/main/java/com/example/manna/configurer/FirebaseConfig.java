2179db49b55aa108825f5d42b3c17fbff3758e32
okane <okanekudasai6@gmail.com>
2024-05-17 07:10:30 +0900

    firebaseinit

--- src/main/java/com/example/manna/configurer/FirebaseConfig.java
+++ src/main/java/com/example/manna/configurer/FirebaseConfig.java
@@ -5,8 +5,8 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -15,18 +15,20 @@ public class FirebaseConfig {
     @Configuration
     public class FirebaseConfiguration {
 
-        private String firebaseSdkPath = "src/main/resources/mannayo-6dcdb-firebase-adminsdk-3pf5g-6230376f83.json";
+        private String firebaseSdkPath = "mannayo-6dcdb-firebase-adminsdk-3pf5g-6230376f83.json";
 
         private FirebaseApp firebaseApp;
 
         @PostConstruct
         public void initializeFCM() throws IOException {
             try {
-                System.out.println("시작합니다 " + firebaseSdkPath);
-                FileInputStream serviceAccount = new FileInputStream(firebaseSdkPath);
-                System.out.println("파일 : " + serviceAccount);
-                FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
-                FirebaseApp.initializeApp(options);
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(
+                                new ClassPathResource(firebaseSdkPath).getInputStream()))
+                        .build();
+                if (FirebaseApp.getApps().isEmpty()) {
+                    FirebaseApp.initializeApp(options);
+                }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
